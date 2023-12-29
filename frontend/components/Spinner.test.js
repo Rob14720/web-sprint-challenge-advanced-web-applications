50bac2f3bec4429d8486bd41937c9723f935d21e
Robert <Roberttsax18@gmail.com>
2023-12-28 20:41:03 -0500

    again?

--- frontend/components/Spinner.test.js
+++ frontend/components/Spinner.test.js
@@ -1,5 +1,13 @@
-// Import the Spinner component into this file and test
-// that it renders what it should for the different props it can take.
-test('sanity', () => {
-  expect(true).toBe(false)
-})
+import React from 'react';
+import { render } from '@testing-library/react';
+import Spinner from './Spinner';
+
+test('Spinner renders correctly with different props', () => {
+  // Test case 1: Spinner is not displayed
+  const { container: spinnerOffContainer } = render(<Spinner on={false} />);
+  expect(spinnerOffContainer.firstChild).toBeNull();
+
+  // Test case 2: Spinner is displayed
+  const { container: spinnerOnContainer } = render(<Spinner on={true} />);
+  expect(spinnerOnContainer.firstChild).toBeTruthy();
+});
